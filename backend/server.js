e6ab3e4a959a290a0b33ee3213793414d1036a64
Caiketadeus <caikecarioca@gmail.com>
2026-05-24 15:58:12 -0300

    email pk

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const validator = require('validator');
 
 const app = express();
 
@@ -9,18 +10,25 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/usuarios', (req, res) => {
-    const { nome , senha } = req.body;
+    const { email, nome , senha } = req.body;
+    if(!email || !validator.isEmail(email)){
+        return res.status(400).json({ mensagem: "O e-mail fornecido é inválido!" });
+    }
+
     if(!nome || !senha){
         return res.status(400).json({mensagem: "Campos obrigatorios vazios"});
     }
 
-    const query = 'INSERT INTO usuarios (nome, senha) VALUES (?, ?)';
-    db.query(query, [nome, senha], (err, result) => {
+    const query = 'INSERT INTO usuarios (email, nome, senha) VALUES (?, ?, ?)';
+    db.query(query, [email, nome, senha], (err, result) => {
         if (err) {
+            if (err.code === 'ER_DUP_ENTRY') {
+            return res.status(400).json({ mensagem: "Este e-mail já está cadastrado!" });
+        }
             console.error('Erro ao inserir no banco: ' + err.stack);
             return res.status(500).json({ mensagem: 'Erro ao salvar o usuario.' });
         }
-        res.json({id: result.insertId, nome, senha});
+        res.json({email, nome, senha});
     });
 });
 
@@ -35,11 +43,11 @@ app.get('/usuarios', (req, res) => {
     })
 })
 
-app.delete('/usuarios/:id', (req, res) => {
-    const { id } = req.params;
-    const query =  'DELETE FROM usuarios WHERE id = ?';
+app.delete('/usuarios/:email', (req, res) => {
+    const { email } = req.params;
+    const query =  'DELETE FROM usuarios WHERE email = ?';
 
-    db.query(query, [id], (err, result) => {
+    db.query(query, [email], (err, result) => {
         if (err) {
             console.error('Erro ao deletar do banco: ' + err.stack);
             return res.status(500).json({ mensagem: 'Erro ao deletar o usuario.' });
--- frontend/script.js
+++ frontend/script.js
@@ -1,35 +1,63 @@
 const apiURL = 'http://localhost:3000/usuarios';
 
 async function cadastrar() {
+    const inpEmail = document.getElementById('iemail');
     const inpNome = document.getElementById('inome');
     const inpSenha = document.getElementById('isenha');
+    const msg = document.getElementById('msg');
+
+    if (!inpEmail.checkValidity()) {
+        msg.innerHTML = "Por favor, insira um e-mail válido.";
+        msg.style.display = 'block';
+        return;
+    }
 
     if (inpSenha.validity.tooShort) {
     msg.innerHTML = `A senha deve ter no mínimo ${inpSenha.minLength} caracteres.`;
     msg.style.display = 'block';
     return; 
-}
+    }
 
-    const novoCadastro = {
+
+const novoCadastro = {
+        email: inpEmail.value,
         nome: inpNome.value,
         senha: inpSenha.value
     };
 
-    await fetch(apiURL, {
+const response = await fetch(apiURL, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(novoCadastro)
     });
-    localStorage.setItem('nomeGuardado', inpNome.value);
-    inpNome.value = '';
-    inpSenha.value = '';
-
     
 
-    setTimeout(() => {
+const resultado = await response.json();
+
+    if (response.status === 400) {
+
+        alert(resultado.mensagem); 
+
+    } else if (response.status === 201) {
+        alert("Usuário cadastrado com sucesso!");
+
+        localStorage.setItem('nomeGuardado', inpNome.value);
+
+        inpEmail.value = '';
+        inpNome.value = '';
+        inpSenha.value = '';
+        if(msg) msg.style.display = 'none';
+
+        setTimeout(() => {
         window.location.replace('telaPrincipal.html');
     }, 500);
-}
+} else{
+        alert("Erro interno no servidor.");
+    }
+
+}  
+
+    
 
 async function logar() {
     const response = await fetch(apiURL);
@@ -52,11 +80,18 @@ async function logar() {
         msg.style.color = 'green';     
         msg.style.display = 'block';
 
-         localStorage.setItem('nomeGuardado', achou.nome);
-        setTimeout(() => {
-            window.location.replace('telaPrincipal.html');
-        }, 1500);
-    }else {
+        localStorage.setItem('nomeGuardado', achou.nome);
+            if(achou.funcao == "usuario"){
+                setTimeout(() => {
+                window.location.replace('telaPrincipal.html');
+                }, 1500); 
+            }else{
+                setTimeout(() => {
+                window.location.replace('listarUsuarios.html');
+                }, 1500); 
+            }
+
+        }else {
         msg.innerHTML = 'Dados incorretos';
         msg.style.color = 'red';      
         msg.style.display = 'block';      
